f98e621a55d8e69de7392581e411d18dcd669e67
GuillermoSanchez11 <guillermo.sanchez1@udea.edu.co>
2024-10-24 21:19:28 -0500

    refactor: update controller

--- Back/src/main/java/com/udea/vueloudea/repository/ITypeRepository.java
+++ Back/src/main/java/com/udea/vueloudea/repository/ITypeRepository.java
@@ -1,6 +1,5 @@
 package com.udea.vueloudea.repository;
 
-import com.udea.vueloudea.model.Flight;
 import com.udea.vueloudea.model.Type;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
