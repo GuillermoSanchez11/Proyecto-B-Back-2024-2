9556ac82bdcc21ccfd2eda7ddc4b9b0e66ccc189
GuillermoSanchez11 <guillermo.sanchez1@udea.edu.co>
2024-10-24 21:19:53 -0500

    refactor: update controller

--- Back/src/main/java/com/udea/vueloudea/repository/IStatusRepository.java
+++ Back/src/main/java/com/udea/vueloudea/repository/IStatusRepository.java
@@ -1,6 +1,5 @@
 package com.udea.vueloudea.repository;
 
-import com.udea.vueloudea.model.Flight;
 import com.udea.vueloudea.model.Status;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
